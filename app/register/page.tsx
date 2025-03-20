0e518c891c6e7226838ceac24a3d221e8b2ec5e0
adamant213-tech <adambern19@gmail.com>
2025-03-19 20:44:30 -0400

    aaa

---
+++ app/register/page.tsx
@@ -0,0 +1,10 @@
+export default function Register() {
+  return (
+    <div>
+      <h1>
+        Register <FaUser size={25} color="green" />
+      </h1>
+      <p>THIS IS A TEST</p>
+    </div>
+  );
+}
