5ac037d7a60972f07b3ea9163f4900bad6b2f496
adamant213-tech <adambern19@gmail.com>
2025-03-19 20:46:00 -0400

    aaaa

--- app/register/page.tsx
+++ app/register/page.tsx
@@ -1,9 +1,9 @@
+import { FaUser } from "react-icons/fa";
+
 export default function Register() {
   return (
     <div>
-      <h1>
-        Register <FaUser size={25} color="green" />
-      </h1>
+      <h1>Register <FaUser /></h1>
       <p>THIS IS A TEST</p>
     </div>
   );
